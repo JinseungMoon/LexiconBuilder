bdf4907db752340d3792ad4c818f3ff90c1ca474
JinseungMoon <mjs0695@gmail.com>
2016-02-19 14:36:38 +0100

    file writing completed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import etr
 import string
+import os
 
 idxList = str('1') + string.lowercase[:26] # 1 for #
+#idxList = str('1')
 termLinks = list()
 
 
@@ -19,26 +21,43 @@ for i in xrange(len(idxList)):
     for termLink in termLinks[i]:
 
         termUrl = ("http://www.investopedia.com"+ termLink)
-        print "\n\n----------------------------------------------------------------"
+        print "\n\n--------------------------------------------------------------------------------"
         print "%s" % (termUrl)
-        print "----------------------------------------------------------------"
+        print "--------------------------------------------------------------------------------"
 
          # extract definition and break down of term
         termContentElements = etr.getSoup(termUrl).find("div", id="Content") # element of id="Content"
         termDefElement = termContentElements.find("div", class_="content-box content-box-term").p 
 
-        print "[Definition]"
-        print repr(termDefElement.text)
+        # write on a txt file
+        curDir = os.getcwd()
+        filePath = curDir + "/data" + termLink.lstrip("terms").rstrip(".asp") + ".txt"
+        linkfilePath = filePath.rstrip(".txt") + "_RT.txt"
+       
+        targetDir = os.path.dirname(filePath)
+        if not os.path.exists(targetDir):
+            os.makedirs(targetDir)
+        
+        f = open(filePath, 'w')
+        fl = open(linkfilePath, 'w')
+       
+        f.write("[Definition]\n")
+        f.write(termDefElement.text.encode('utf-8'))
 
-        print "\n[Break Down]"
-        print repr(termDefElement.find_next_siblings('p')[0].text)
+        f.write("\n\n[Break Down]\n")
+        f.write(termDefElement.find_next_siblings('p')[0].text.encode('utf-8'))
 
-        print "\n[Links]"
+        # print "\n[Links]"
         relatedLinks = etr.getHrefs(termContentElements, "div", "box below-box col-2 no-image gray clear")
         for link in relatedLinks:
-            print link
+            # print link
+            fl.write(link + '\n')
 
         termCount += len(termLinks)
+        f.close()
+        fl.close()
+        print '\'' + filePath + '\'' + ' completed'
+        print '\'' + linkfilePath + '\''+ ' completed'
 
     print ("total number pages from alphabet '%s' = %d\n") % (idxList[i], termCount)
 
