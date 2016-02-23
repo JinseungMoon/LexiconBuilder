b76d2f09dad355386fb673534af27c9d8c7e302b
JinseungMoon <mjs0695@gmail.com>
2016-02-23 15:53:07 +0100

    feedback applied

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ import codecs
 import requests
 import Queue
 import threading
-from multiprocessing.dummy import Pool as ThreadPool
 import time
 from bs4 import BeautifulSoup
 from datetime import datetime
 
 # -----------------------------------------------------------------------------------
-# ------------Global values
+# ------------Global variables
 # -----------------------------------------------------------------------------------
 baseUrl = "http://www.investopedia.com"
 letterList = '1abcdefghijklmnopqrstuvwxyz'
-#letterList = 'defg1qrz'
+# letterList = '1'
 outDir = os.getcwd() + "/out/"
 lock = threading.Lock()
 dictionary = list()
+fetchedCount = 0
 totalCount = 0
 warnCount = 0
-
+###########https://docs.python.org/2/howto/logging.html
 
 # -----------------------------------------------------------------------------------
 # ------------Functions
@@ -31,7 +31,6 @@ def getSoup( url ):
     page = requests.get(url).text
     soup = BeautifulSoup(page, "lxml")
     return soup
-
 
 # get links of a term as string type
 def getTermLinks( letter ):
@@ -59,28 +58,34 @@ def getTermLinks( letter ):
         for link in links:
             termLinks.append(link.get("href"))
 
-    print "%s '%d' links fetched.\n" % (baseUrl, len(termLinks))
+    print "%s '%d' links fetched." % (baseUrl, len(termLinks))
+    
+    global fetchedCount
+    with lock:
+    	fetchedCount += len(termLinks)
+
     dictionary.append(termLinks)
 
 def extractContent(idx):
  	for termLink in dictionary[idx]:
 
 		termUrl = baseUrl + termLink
-		#termUrl = "http://www.investopedia.com/terms/p/ppipla.asp"
+		# termUrl = "http://www.investopedia.com/terms/w/workers-compensation.asp"
 		
-		print "%s" % (termUrl) 
+		print termUrl
 
 		# write on a txt file
 		filePath = outDir + termUrl[len(baseUrl)+1:].rstrip(".asp") + ".txt"
-		linkfilePath = filePath.rstrip(".txt") + "_RT.txt"
+		anchorFilePath = filePath.rstrip(".txt") + "_IL.txt"
+		linkFilePath = filePath.rstrip(".txt") + "_RT.txt"
 
 		targetDir = os.path.dirname(filePath)
 		if not os.path.exists(targetDir):
 		    os.makedirs(targetDir)
 
 		f = codecs.open(filePath, encoding='utf-8', mode='w')
-		fl = codecs.open(linkfilePath, encoding='utf-8', mode='w')
-
+		al = codecs.open(anchorFilePath, encoding='utf-8', mode='w')
+		fl = codecs.open(linkFilePath, encoding='utf-8', mode='w')
 
 		 # extract definition and break down of term
 		soup = getSoup(termUrl)
@@ -90,26 +95,38 @@ def extractContent(idx):
 		if content == None:
 			logf.write(str('[Invalid Format#1!]' + termLink + '\n'))
 			f.close()
+			al.close()
 			fl.close()
 			continue
 
-		termDefElements = content.find_all('p') 
+		tags = content.find_all(['h2','p'])
+		hIdx = 0  # head index
+		pIdx = 0  # paragraph index
+		pCharCounts = 0  # counts of characters in a paragraph
+		for tag in tags:
 
-		if len(termDefElements) <= 1:
-			logf.write(str('[Invalid Format#2!]' + termLink + '\n'))
-			with lock:
-				global warnCount
-				warnCount += 1
+			if len(tags) <= 1:
+				logf.write(str('[Invalid Format#2!]' + termLink + '\n'))
+				with lock:
+					global warnCount
+					warnCount += 1
 
-		for num in xrange(len(termDefElements)):
-			if num == 0:
-				f.write("[Definition]\n")
-				f.write(termDefElements[0].text)
-			elif num == 1:	
-				f.write("\n\n[Break Down]\n")
-				f.write(termDefElements[1].text)
+			if tag.name == 'h2':
+				pIdx = 0
+				hIdx += 1
+				f.write('\n[' + tag.text + ']\n' )
+			if tag.name == 'p':
+				pIdx += 1
+				pCharCounts = len(tag.text)
+				f.write(tag.text)
+				f.write('counts:%d\n' % pCharCounts )
+				anchors = tag.find_all('a')
+				for anchor in anchors:
+					idxStart = tag.text.find(anchor.text)
+					al.write('[head#%d:para#%d(%d)]' % (hIdx, pIdx, pCharCounts) )
+					al.write(anchor.text + ' ' + unicode(anchor.get("href")) + ' ' + str(idxStart) + '\n')
 
-
+		
 		# extract related links
 		relatedLinks = soup.find("div", class_="box below-box col-2 no-image gray clear").find_all("a")
 		for relatedLink in relatedLinks:
@@ -120,14 +137,14 @@ def extractContent(idx):
 			totalCount += len(termLink)
 
 		f.close()
+		al.close()
 		fl.close()
-	
-		
+			
 def worker():
-    while True:
-        letter = q.get()
-        getTermLinks(letter)
-        q.task_done()
+	    while True:
+	        letter = q.get()
+	        getTermLinks(letter)
+	        q.task_done()
 
 def worker2():
     while True:
@@ -165,9 +182,15 @@ for i in xrange(len(dictionary)):
 
 q2.join()
 
+# print out result
+print "\n# of fetched Pages: %d" % fetchedCount
+print "# of Witten Pages: %d" % totalCount
+print "# of Warnnings: %d\n\n" % warnCount
+
 # write result data
 logf.write("--------------------------------------------------------------------\n")
 logf.write(str(datetime.now())+'\n')
-logf.write("\n# of Pages: %d" % totalCount)
+logf.write("\n# of fetched Pages: %d" % fetchedCount)
+logf.write("\n# of Witten Pages: %d" % totalCount)
 logf.write("\n# of Warnnings: %d\n\n" % warnCount)
 logf.close()
